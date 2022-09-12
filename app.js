0db3edfa59ead7ca7f8076e413a5406ce7c0ddd3
LaLacou <108777796+users.noreply.github.com>
2022-09-11 17:12:32 -0700

    added question validation

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'What is your GitHub name?'
+            message: 'What is your GitHub name? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your Username!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -36,7 +52,15 @@ if (!portfolioData.projects) {
         {
             type: 'input',
             name: 'projectname',
-            message: "What is the name of your project?"
+            message: "What is the name of your project? (Required)",
+            validate: projectnameInput => {
+                if (projectnameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your Project name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -53,7 +77,15 @@ if (!portfolioData.projects) {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. (Required)'
+            message: 'Enter the Github link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
